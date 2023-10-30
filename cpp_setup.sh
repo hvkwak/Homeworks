15ba6687039c7ada2488a5bc40da30c20e4e43a9
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2023-10-30 22:13:19 +0100

    Update cpp_setup.sh

--- cpp_setup.sh
+++ cpp_setup.sh
@@ -2,16 +2,17 @@
 # run this script to setup cpp development environment
 # this includes installation of LLVM, Clang
 
-CLANG_VERSION=16
+version=16
+priority=100
 
 # install llvm
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh $CLANG_VERSION all
+sudo ./llvm.sh $version all
 
 # Clang and co
-sudo apt-get install clang-${CLANG_VERSION} clang-tools-${CLANG_VERSION} clang-${CLANG_VERSION}-doc libclang-common-${CLANG_VERSION}-dev libclang-${CLANG_VERSION}-dev libclang1-${CLANG_VERSION} clang-format-${CLANG_VERSION} python3-clang-${CLANG_VERSION} clangd-${CLANG_VERSION} clang-tidy-${CLANG_VERSION}
+sudo apt-get install clang-${version} clang-tools-${version} clang-${version}-doc libclang-common-${version}-dev libclang-${version}-dev libclang1-${version} clang-format-${version} python3-clang-${version} clangd-${version} clang-tidy-${version}
 
 # lldb
 sudo apt-get install lldb-16
@@ -21,3 +22,57 @@ sudo apt-get install lld-16
 sudo apt-get install libc++-16-dev libc++abi-16-dev
 # OpenMP
 sudo apt-get install libomp-16-dev
+
+
+function register_clang_version {
+
+    update-alternatives \
+        --verbose \
+        --install /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-${version} ${priority} \
+        --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-${version} \
+        --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-${version} \
+        --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-${version} \
+        --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-${version} \
+        --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-${version} \
+        --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-${version} \
+        --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-${version} \
+        --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-${version} \
+        --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-${version} \
+        --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-${version} \
+        --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-${version} \
+        --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-${version} \
+        --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-${version} \
+        --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-${version} \
+        --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-${version} \
+        --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-${version} \
+        --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-${version} \
+        --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-${version} \
+        --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-${version} \
+        --slave   /usr/bin/llvm-objcopy      llvm-objcopy     /usr/bin/llvm-objcopy-${version} \
+        --slave   /usr/bin/llvm-strip        llvm-strip       /usr/bin/llvm-strip-${version}
+
+    update-alternatives \
+        --verbose \
+        --install /usr/bin/clang                 clang                 /usr/bin/clang-${version} ${priority} \
+        --slave   /usr/bin/clang++               clang++               /usr/bin/clang++-${version}  \
+        --slave   /usr/bin/asan_symbolize        asan_symbolize        /usr/bin/asan_symbolize-${version} \
+        --slave   /usr/bin/clang-cpp             clang-cpp             /usr/bin/clang-cpp-${version} \
+        --slave   /usr/bin/clang-check           clang-check           /usr/bin/clang-check-${version} \
+        --slave   /usr/bin/clang-cl              clang-cl              /usr/bin/clang-cl-${version} \
+        --slave   /usr/bin/ld.lld                ld.lld                /usr/bin/ld.lld-${version} \
+        --slave   /usr/bin/lld                   lld                   /usr/bin/lld-${version} \
+        --slave   /usr/bin/lld-link              lld-link              /usr/bin/lld-link-${version} \
+        --slave   /usr/bin/clang-format          clang-format          /usr/bin/clang-format-${version} \
+        --slave   /usr/bin/clang-format-diff     clang-format-diff     /usr/bin/clang-format-diff-${version} \
+        --slave   /usr/bin/clang-include-fixer   clang-include-fixer   /usr/bin/clang-include-fixer-${version} \
+        --slave   /usr/bin/clang-offload-bundler clang-offload-bundler /usr/bin/clang-offload-bundler-${version} \
+        --slave   /usr/bin/clang-query           clang-query           /usr/bin/clang-query-${version} \
+        --slave   /usr/bin/clang-rename          clang-rename          /usr/bin/clang-rename-${version} \
+        --slave   /usr/bin/clang-reorder-fields  clang-reorder-fields  /usr/bin/clang-reorder-fields-${version} \
+        --slave   /usr/bin/clang-tidy            clang-tidy            /usr/bin/clang-tidy-${version} \
+        --slave   /usr/bin/lldb                  lldb                  /usr/bin/lldb-${version} \
+        --slave   /usr/bin/lldb-server           lldb-server           /usr/bin/lldb-server-${version} \
+        --slave   /usr/bin/clangd                clangd                /usr/bin/clangd-${version}
+}
+
+register_clang_version $version $priority
