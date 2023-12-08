15aeca3ef7b74df16c26ceb392660b0acf937d19
Hyobin <hyovin.kwak@tu-dortmund.de>
2023-12-08 18:53:06 +0100

    file change init.el

--- setup/init.el
+++ setup/init.el
@@ -1,1 +1,115 @@
-/home/hyobin/.emacs.d/init.el
+;;; init.el --- Hyovin's Emacs init
+;;; Commentary:
+;;; based on dfrib's Emacs init:
+;;; https://github.com/dfrib/emacs_setup/blob/master/.emacs.d/init.el
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MELPA                                                                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Bootstrap `use-package' and packages                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; enable yasnippet                                                             ;;
+;; yasnippet-snippets - a snippet collection package maintained by AndreaCrotti.;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package yasnippet
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t)
+  (yas-global-mode t)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; enable flycheck and                                                            ;;
+;; enable eglot (configure eglot to start automatically)                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package flycheck
+  :ensure t
+  :after yasnippet
+  :config
+  (global-flycheck-mode t)
+  )
+
+;; project-cmake from:
+;; https://github.com/juanjosegarciaripoll/project-cmake
+(use-package project-cmake
+  :load-path "/home/hyobin/.emacs.d/project-cmake/"
+  :after yasnippet
+  :config
+  (require 'eglot)
+  (project-cmake-scan-kits)
+  (project-cmake-eglot-integration)
+  (setq project-cmake-kit 'unix-clang)
+  (setq project-cmake-build-directory-name (concat default-directory "./build"))
+  )
+
+
+(use-package eglot
+  :ensure t
+  :after project-cmake
+  :config
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+  (add-hook 'c-mode-hook 'eglot-ensure)
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+  )
+
+(use-package flycheck-eglot
+  :ensure t
+  :after (flycheck eglot)
+  :config
+  (global-flycheck-eglot-mode 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; small interface tweaks                                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "<f4>") 'revert-buffer)
+(global-display-line-numbers-mode 1) ;;; Display line numbers in every buffer
+;; set tabs to indent as white spaces and set default tab width to 4 white spaces
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+;(setq-default indent-line-function 'insert-tab)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; enable modus themes                                                           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "/home/hyobin/.emacs.d/modus-themes")
+(require 'modus-themes) ; dark theme
+
+;; Add all your customizations prior to loading the themes
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil)
+;; Load the theme of your choice.
+(load-theme 'modus-vivendi :no-confirm)
+(define-key global-map (kbd "<f5>") #'modus-themes-toggle)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
+ '(package-selected-packages
+   '(flycheck-eglot flycheck dash flymake yasnippet-snippets yasnippet eglot compat cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
